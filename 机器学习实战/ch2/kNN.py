064485c3a1fa44dc76cb04b01045a29dade71527
任欣源 <245735349@qq.com>
2017-11-01 15:13:30 +0800

    机器学习实战代码改进
    第二章学习与改进

--- 机器学习实战/ch2/kNN.py
+++ 机器学习实战/ch2/kNN.py
@@ -1,16 +1,3 @@
-'''
-Created on Sep 16, 2010
-kNN: k Nearest Neighbors
-
-Input:      inX: vector to compare to existing dataset (1xN)
-            dataSet: size m data set of known vectors (NxM)
-            labels: data set labels (1xM vector)
-            k: number of neighbors to use for comparison (should be an odd number)
-            
-Output:     the most popular class label
-
-@author: pbharrin
-'''
 from numpy import *
 import operator
 from os import listdir
@@ -32,7 +19,7 @@ def classify0(inX, dataSet, labels, k):
         voteIlabel = labels[sortedDistIndicies[i]]
         #如果没有则返回0
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     #返回出现频率最高的label值和频率
     return sortedClassCount[0][0]
 
@@ -41,6 +28,7 @@ def createDataSet():
     labels = ['A','A','B','B']
     return group, labels
 
+#讲文本记录转化为numpy
 def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())         #get the number of lines in the file
@@ -49,25 +37,33 @@ def file2matrix(filename):
     fr = open(filename)
     index = 0
     for line in fr.readlines():
+        #去掉左右两边的空格，'\n'
         line = line.strip()
+        #按照tab键进行分割
         listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
+        #将数据插入改列
+        returnMat[index,:] = listFromLine[0:3]   
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat,classLabelVector
-    
+
+#归一化特征值 公式：newvalue = (oldvalue-min)/(max-min)
 def autoNorm(dataSet):
+    #无参，所有中的最小值   axis=0; 每列的最小值  axis=1；每行的最小值 
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
     normDataSet = zeros(shape(dataSet))
     m = dataSet.shape[0]
+    #将min和max转为n行一列的向量
     normDataSet = dataSet - tile(minVals, (m,1))
     normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
     return normDataSet, ranges, minVals
-   
-def datingClassTest():
-    hoRatio = 0.50      #hold out 10%
+  
+#对约会网站进行测试
+def datingClassTest(hoRatio):
+    #0.1为测试集 0.9为训练集
+    #hoRatio = 0.10      #hold out 10%
     datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
     normMat, ranges, minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
@@ -75,10 +71,11 @@ def datingClassTest():
     errorCount = 0.0
     for i in range(numTestVecs):
         classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        #print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]): errorCount += 1.0
     print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
-    print(errorCount)
+    return (errorCount/float(numTestVecs))
+    #print(errorCount)
     
 def img2vector(filename):
     returnVect = zeros((1,1024))
